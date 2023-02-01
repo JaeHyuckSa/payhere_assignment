3a21941a81bb1779e5080a6c410c7bcd592b9bfd
ssa <wogur981208@gmail.com>
2023-02-01 18:03:23 +0900

    :sparkles:Feat: 회원가입 기능 추가 #1
    -1. urls.py(users/auth/signup/)
    -2. views.py(post요청으로 유저 생성되도록 만듬)
    -3. validators.py(비밀번호와 닉네임 정규표현식으로 유효성검사 함수 생성)
    -4. serializers.py(비밀번호와 재확인, 닉네임, 이메일관련 직렬화)
    related_to: #1

---
+++ users/serializers.py
@@ -0,0 +1,84 @@
+# rest_framework
+from rest_framework import serializers
+
+
+# users
+from .models import User
+from .validators import (
+    password_validator,
+    nickname_validator,
+)
+
+
+# 회원가입 serializer
+class SignupSerializer(serializers.ModelSerializer):
+    repassword = serializers.CharField(
+        error_messages={
+            "required": "비밀번호를 입력해주세요.",
+            "blank": "비밀번호를 입력해주세요.",
+            "write_only": True,
+        }
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "nickname",
+            "password",
+            "repassword",
+            "email",
+        )
+        extra_kwargs = {
+            "nickname": {
+                "error_messages": {
+                    "required": "닉네임을 입력해주세요.",
+                    "blank": "닉네임을 입력해주세요",
+                }
+            },
+            "password": {
+                "write_only": True,
+                "error_messages": {
+                    "required": "비밀번호를 입력해주세요.",
+                    "blank": "비밀번호를 입력해주세요.",
+                },
+            },
+            "email": {
+                "error_messages": {
+                    "required": "이메일을 입력해주세요.",
+                    "invalid": "알맞은 형식의 이메일을 입력해주세요.",
+                    "blank": "이메일을 입력해주세요.",
+                }
+            },
+        }
+
+    def validate(self, data):
+        nickname = data.get("nickname")
+        password = data.get("password")
+        repassword = data.get("repassword")
+
+        # 닉네임 유효성 검사
+        if nickname_validator(nickname):
+            raise serializers.ValidationError(detail={"nickname": "닉네임은 3자이상 10자 이하로 작성해야하며 특수문자는 포함할 수 없습니다."})
+
+        # 비밀번호 일치
+        if password != repassword:
+            raise serializers.ValidationError(detail={"password": "비밀번호가 일치하지 않습니다."})
+
+        # 비밀번호 유효성 검사
+        if password_validator(password):
+            raise serializers.ValidationError(detail={"password": "비밀번호는 8자 이상 16자이하의 영문 소문자, 숫자, 특수문자 조합이어야 합니다."})
+
+        return data
+
+    def create(self, validated_data):
+        nickname = validated_data["nickname"]
+        email = validated_data["email"]
+
+        user = User(
+            nickname=nickname,
+            email=email,
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+
+        return user
