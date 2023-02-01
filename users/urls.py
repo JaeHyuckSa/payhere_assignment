93f74873626d627a7775e7429285d7c116453234
ssa <wogur981208@gmail.com>
2023-02-01 20:09:56 +0900

    :sparkles:Feat: 토큰 유효한지 확인하는 기능 추가 #1
    -1. urls.py(users/auth/verify/)
    refresh, access token이 유효하면 200 status code, 유효하지 않으면 401
    status code 반환
    related_to: #1

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 # rest_framework_simplejwt
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 # django
 from django.urls import path
@@ -14,4 +14,7 @@ urlpatterns = [
     # Login
     path("signin/", views.CustomTokenObtainPairView.as_view(), name="auth-signin"),
     path("signin/refresh/", TokenRefreshView.as_view(), name="auth-signin-refresh"),
+    
+    # Token verify
+    path("verify/", TokenVerifyView.as_view(), name="auth-verify"),
 ]
