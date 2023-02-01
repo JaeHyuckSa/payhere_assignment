1d6b250ccdb3eca31c1651cc8d7ef62069493e90
ssa <wogur981208@gmail.com>
2023-02-01 20:22:40 +0900

    :sparkles:Feat: 로그아웃 기능 추가 #1
    -1. urls.py(users/auth/signout)
    -2. serializers.py(token을 blacklist에 저장해버림)
    -2. views.py(post 요청으로 refresh 토큰을 보냄)
    related_to: #1

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import  AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # rest_framework_simplejwt
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -14,6 +14,7 @@ from drf_yasg.utils import swagger_auto_schema
 from .serializers import (
     CustomTokenObtainPairSerializer,
     SignupSerializer,
+    SignoutSerializer,
 )
 
 
@@ -35,3 +36,19 @@ class SingupView(APIView):
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+
+class SignoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+        request_body=SignoutSerializer,
+        operation_summary="로그아웃",
+        responses={200: "성공", 400: "토큰 유효하지 않음", 401: "인증 에러", 500: "서버 에러"},
+    )
+    def post(self, request):
+        serializer = SignoutSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "로그아웃이 되었습니다."}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
