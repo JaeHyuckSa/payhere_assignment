d44fbfcf7c8192f324152c193f8a357843e57509
ssa <wogur981208@gmail.com>
2023-02-04 16:53:22 +0900

    :rocket:MySQL 설정 #5
    -1. AWS RDS 와 연동하기 위해 MYSQL을 설정했습니다
    related_to: #5

--- payhere/settings.py
+++ payhere/settings.py
@@ -2,7 +2,8 @@ from pathlib import Path
 import os
 import environ
 from datetime import timedelta
-
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,13 +82,31 @@ WSGI_APPLICATION = "payhere.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+MYSQL_DB = env("MYSQL_DB")
+if MYSQL_DB:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': MYSQL_DB,
+            'USER': env("MYSQL_USER"),
+            'PASSWORD': env("MYSQL_PASSWORD"),
+            'HOST': env("MYSQL_HOST"),
+            'PORT':env("MYSQL_PORT"),
+            'OPTIONS':{
+                'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+            'TEST' : {
+            'NAME' : BASE_DIR / "db.sqlite3",
+            },
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
